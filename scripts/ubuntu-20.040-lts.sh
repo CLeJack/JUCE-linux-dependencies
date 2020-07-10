d0f99404323292f98cfc461cd9d358759be3e38d
CLeJack <cjacksonv3.0@gmail.comgit config --global user.name CLeJack>
2020-07-10 11:21:00 -0500

    first commit

---
+++ scripts/ubuntu-20.040-lts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+# ubuntu studio 20.04; juce 6.0.1
+# Juce Settings -> Header Search Paths; add /usr/include
+sudo apt install -y libasound2-dev libfreetype6-dev libx11-dev libxinerama-dev libxrandr-dev libxcursor-dev mesa-common-dev freeglut3-dev libxcomposite-dev libcurl4-gnutls-dev ladspa-sdk libwebkit2gtk-4.0-dev libgtk-3-dev
+
